6767d7a4d8c53009d0268ab12fdbe8d6acda2bcf
Ahsan Abid <ahsanabid52@gmail.com>
2018-10-01 17:07:00 +0400

    Removing setups

---
+++ Projects/cmp-databinding-start/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [{type:'server',name:'First Server',content:'Empty'}];
+
+  onServerAdded(serverData:{serverName:string,serverContent:string}) {
+    this.serverElements.push({
+      type:'server',
+      name:serverData.serverName,
+      content:serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprint:{serverName:string,serverContent:string}) {
+    this.serverElements.push({
+      type:'blueprint',
+      name:blueprint.serverName,
+      content:blueprint.serverContent
+    });
+}
+}
