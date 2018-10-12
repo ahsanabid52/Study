e0154165db81b97b81cd8765dd5cb63bb59db881
Ahsan Abid <ahsanabid52@gmail.com>
2018-10-12 04:03:30 +0400

    Logging issues are corrected

--- Projects/SpringBoot/myTaxi/src/main/java/com/mytaxi/domainvalue/CarStatus.java
+++
@@ -1,8 +0,0 @@
-package com.mytaxi.domainvalue;
-
-public enum CarStatus
-{
-    AVAILABLE, /*Car is available for the driver to start a journey.*/
-
-    ON_THE_ROAD/*Car is on the road which means already some drive is driving it. */
-}
